d7e2ba61c9f60cd25680470f178338e07ff6b7d6
kojoakyer <kojoakyer777@gmail.com>
2023-08-16 10:46:06 -0700

    updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by kojo
-	 * Return: 0
-	 */
+/**
+* main - print _putchar
+* is written by kojo
+* Return: 0
+*/
 
 
 	int main(void)
