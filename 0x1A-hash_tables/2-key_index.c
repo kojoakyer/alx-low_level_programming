ed2a5aeb89117e4ab6a019a85da62e3e212d7a0e
Kojo Akyer <kojoakyer777@gmail.com>
2024-02-02 10:05:38 -0800

    updates

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- *  * key_index - get index for key given
- *   * @key: key (string) to hash
- *    * @size: size  hash table
- *     *
- *      * Return: index 'key' using djb2
- *       */
+ * key_index - get index for key given
+ * @key: key (string) to hash
+ * @size: size  hash table
+ *
+ * Return: index 'key' using djb2
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
